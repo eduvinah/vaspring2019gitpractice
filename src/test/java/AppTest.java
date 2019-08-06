b143cc3343b311df0091c149febae25d6701e0df
eduvinah <50126976+eduvinah@users.noreply.github.com>
2019-08-06 19:31:54 -0400

    Update AppTest.java

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,8 +1,7 @@
 public class AppTest {
     public static void main(String[] args) {
         System.out.println("hello");
-        System.out.println("hello");
-        System.out.println("hello");
+        System.out.priptln("hello");
 
 
 
